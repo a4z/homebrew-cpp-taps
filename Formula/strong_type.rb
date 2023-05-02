7943f97ae95b106d385470d059632de29b0c0a84
Harald <harald.achitz@gmail.com>
2023-05-02 08:58:06 +0200

    Add strong_type (#1)
    Add strong_type and do the first CI runs

---
+++ Formula/strong_type.rb
@@ -0,0 +1,34 @@
+class StrongType < Formula
+  desc "Additive strong typedef library for C++14/17/20"
+  homepage "https://github.com/rollbear/strong_type"
+  url "https://github.com/rollbear/strong_type/archive/refs/tags/v10.tar.gz"
+  sha256 "154e4ceda6cf8fe734deb7eafdf58df5052822d04425dc7c22711ef54cdaeefa"
+  license "BSL-1.0"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "016556278cd2880486c4fd1f15918c5561c8034a69c66d80f5a7c9f813381fe1"
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <strong_type/strong_type.hpp>
+      int main() {
+        strong::type<int, struct Tag> i{42};
+        if (value_of(i) == 42)
+          return 0;
+        return 1;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-std=c++14", "-o", "test"
+    system "./test"
+  end
+end
