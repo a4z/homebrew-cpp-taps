778db7505b9e1811c67e194d4f13f47335d1780e
a4z <harald.achitz@gmail.com>
2023-05-08 10:18:01 +0200

    Update to todays master
    Closes #7.
    
    Signed-off-by: Harald <6172295+a4z@users.noreply.github.com>

--- Formula/mdspan.rb
+++ Formula/mdspan.rb
@@ -1,17 +1,12 @@
 class Mdspan < Formula
   desc "Reference mdspan implementation"
   homepage "https://github.com/kokkos/mdspan"
-  url "https://github.com/kokkos/mdspan/archive/refs/tags/mdspan-0.6.0.tar.gz"
-  sha256 "79f94d7f692cbabfbaff6cd0d3434704435c853ee5087b182965fa929a48a592"
+  url "https://github.com/kokkos/mdspan.git", revision: "bca0e55"
+  version "0.6.1-git"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
-  depends_on "python3" => :build
-
   def install
-    include_dir = "#{include}/mdspan"
-    mkdir_p include_dir
-    system "./make_single_header.py ./include/experimental/mdarray > mdspan.hpp"
-    include.install "mdspan.hpp" => "mdspan/"
+    cp_r "include/.", include.to_s
   end
 
   test do
@@ -20,7 +15,7 @@ class Mdspan < Formula
       #include <iostream>
       #include <array>
       int main() {
-        std::array<int> d{
+        std::array<int, 9> d{
           0, 5, 1,
           3, 8, 4,
           2, 7, 6,
@@ -29,7 +24,7 @@ class Mdspan < Formula
         Kokkos::mdspan m{d.data(), Kokkos::extents{3, 3}};
         for (std::size_t i = 0; i < m.extent(0); ++i)
           for (std::size_t j = 0; j < m.extent(1); ++j)
-            std::cout << "m(" << i << ", " << j << ") == " << m(i, j) << "\n";
+            std::cout << "m(" << i << ", " << j << ") == " << m(i, j) << std::endl;
       }
     EOS
     system ENV.cxx, "test.cpp", "-std=c++17", "-o", "test"
